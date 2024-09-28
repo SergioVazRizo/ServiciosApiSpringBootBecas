3375c5b5a5242eda1a0090f2fa3b9c5a01805316
Sergio Vázquez <138184485+SergioVazRizo@users.noreply.github.com>
2024-09-28 14:40:18 -0600

    cambios del actualizar

--- src/main/java/com/becas/becas/Controller/SolicitudBecaController.java
+++ src/main/java/com/becas/becas/Controller/SolicitudBecaController.java
@@ -1,12 +1,15 @@
 package com.becas.becas.Controller;
 
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -40,4 +43,25 @@ public class SolicitudBecaController {
     public void deleteSolicitud(@PathVariable Long idSolicitudBeca){
         solicitudBecaService.delete(idSolicitudBeca);
     }
+
+    @PutMapping("/{idSolicitudBeca}")
+    public SolicitudBeca updateSolicitudBeca(
+            @PathVariable Long idSolicitudBeca, 
+            @RequestBody Map<String, String> updates) {
+        
+        SolicitudBeca solicitudBeca = solicitudBecaService.getSolicitudes(idSolicitudBeca).orElseThrow(() -> new RuntimeException("Solicitud no encontrada"));
+
+        if (updates.containsKey("estadoSolicitud")) {
+            solicitudBeca.setEstadoSolicitud(updates.get("estadoSolicitud"));
+        }
+
+        if (updates.containsKey("comentariosSolicitud")) {
+            solicitudBeca.setComentariosSolicitud(updates.get("comentariosSolicitud"));
+        }
+
+        solicitudBecaService.sevaOrUpdate(solicitudBeca);
+
+        return solicitudBeca;
+    }
+
 }
