bde7a5acd380ccdffda1f8738785a2089cf36858
Sergio Vázquez <138184485+SergioVazRizo@users.noreply.github.com>
2024-10-15 20:33:46 -0600

    Update WebConfig.java

--- src/main/java/com/becas/becas/WebConfig.java
+++ src/main/java/com/becas/becas/WebConfig.java
@@ -17,14 +17,19 @@ public class WebConfig implements WebMvcConfigurer {
             // Añade más orígenes según sea necesario
     };
 
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/api/**")
-                .allowedOrigins(ALLOWED_ORIGINS)
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                .allowedHeaders("*")
-                .allowCredentials(true)
-                .maxAge(3600);
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")  // Cambiado de "/api/**" a "/**"
+                        .allowedOrigins(ALLOWED_ORIGINS)
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(true)
+                        .maxAge(3600);
+            }
+        };
     }
 }
 
